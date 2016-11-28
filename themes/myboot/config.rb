502d46d9c80eb718723b2ddea97ea19c0979c024
Nikita <nik.europe@gmail.com>
2016-11-28 16:29:53 +0100

    Update config.rb

--- themes/myboot/config.rb
+++ themes/myboot/config.rb
@@ -6,5 +6,7 @@ javascripts_dir = "js"
 fonts_dir = "bootstrap/fonts/bootstrap" 
 generated_images_dir = "img" 
 
+
+
 #http_images_path = http_path + "/" + generated_images_dir 
 #http_generated_images_path = http_images_path output_style = (environment == :production) ? :compressed : :expanded 
